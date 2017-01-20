808c77176d4496406916dacf34fc515289ccca4e
lzh <850482461@qq.com>
2017-01-20 17:34:59 +0800

    flask模版学习

---
+++ web/demo.py
@@ -0,0 +1,31 @@
+# -*- code:utf-8 -*-
+from flask_script import Manager
+from flask import Flask,render_template
+from markdown import markdown
+from functools import reduce
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',title="<h1>Hello world!!</h1>",body="##header2##")
+
+#自己定义一个jinjia2的过滤器
+@app.template_filter('md')
+def markdown_to_html(txt):
+    return markdown(txt)
+
+#定义一个函数
+def read_md(filename):
+    #重点：不管是写入还是读取出现编码错误就在open函数添加encodeing
+    with open(filename,encoding='utf-8') as f:
+        content = reduce(lambda x, y: x + y, f.readlines())
+        return content#.decode("utf-8")
+
+#上下文处理器函数
+@app.context_processor
+def jnject_method():
+    return dict(read_md=read_md)
+
+if __name__ =="__main__":
+    app.run()
--- web/hello.py
+++ web/hello.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,make_response,abort
 from werkzeug.routing import BaseConverter
 from werkzeug.utils import secure_filename
 from os import path
+
+
 
 class RegexConverter(BaseConverter):#正则转换器
     def __init__(self,url_map,*items):
@@ -11,17 +13,21 @@ class RegexConverter(BaseConverter):#正则转换器
 app = Flask(__name__)
 app.url_map.converters['regex'] = RegexConverter#正则转换器
 
-@app.route('/')#装饰起用于 根目录
+
+
+@app.route('/')#装饰起用于根目录
 def index():
-    return '<h1>这是主页</h1>'
+    response = make_response('<h1>这是主页,没错!</h1>')
+    response.set_cookie('username','')#cookie
+    return response
 
 
 @app.route('/<name>') #hello目录
 def hello(name):
-    return render_template('hello.html',name=request.method)
+    return render_template('hello.html',name=name)
 
 
-@app.route('/user/<regex("[a-z]{3}"):user_id>')
+@app.route('/user/<regex("[a-z]{3}"):user_id>')#正则表达式验证url
 def user(user_id):
     return 'User {0}'.format(user_id)
 
@@ -37,22 +43,29 @@ def projects():
 @app.route('/login',methods=['GET','POST'])#http方法
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username']#对应的是login的name标签获取里面的内容
         password = request.form['password']
-    else:
-        username = request.args['username']
-
+        print(username,password)#获取前端post传过来的参数可以进行一些验证处理等
+    # else:#如果是用get方法就用这个方法获取前端穿进来的数据
+    #     username = request.args['username']
     return render_template('login.html',method=request.method)
 
-@app.route('/upload',methods=['GET','POST'])#上传文件
+@app.route('/upload',methods=['GET','POST'])#上传文件的http方法
 def upload():
     if request.method=='POST':
-        f = request.files['file']
+        f = request.files['file']#这里的的键值file对应upload.html的name="file"
         basepath = path.abspath(path.dirname(__file__))#返回path规范化的绝对路径
-        upload_path = path.join(basepath, 'static/uploads/')#拼接路径
-        f.save(upload_path+secure_filename(f.filename))
-        return redirect(url_for('upload'))
+        upload_path = path.join(basepath,'static/')#拼接路径
+        f.save(upload_path+secure_filename(f.filename))#检验上传文件并且保存
+        return redirect(url_for('upload'))#指定为postback
     return render_template('upload.html')
 
+@app.errorhandler(404)#自己定义一个错误页面传入错误代码
+def page_not_found(error):
+    return render_template('404.html'),404
+
+
+
 if __name__ == '__main__':
+
     app.run(port=80,debug = True)
