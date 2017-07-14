15798b34c2a017a7130e36206e24edaf28cf15e1
lzh <850482461@qq.com>
2017-07-14 21:07:47 +0800

    改动了一个测试的flask

---
+++ work/flask_test/__init__.py
@@ -0,0 +1,16 @@
+from flask_script import Manager
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+manager = Manager()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:lzh3101977@192.168.1.55:3306/cmdb'
+    db.init_app(app)
+
+    return app
+
+
